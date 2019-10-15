de4b0b788280818fc4eeb8b2ac86e29311551d34
MaxRybalkin91 <granirock@gmail.com>
2019-10-15 21:47:45 +0300

    HW3. Option 2 is realised

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
@@ -18,12 +17,6 @@ import static ru.javawebinar.topjava.UserTestData.USER;
 public class InMemoryUserRepository extends InMemoryBaseRepository<User> implements UserRepository {
     static final int USER_ID = 1;
     static final int ADMIN_ID = 2;
-    private InMemoryUserRepository repository;
-
-    @Autowired
-    public InMemoryUserRepository(InMemoryUserRepository repository) {
-        this.repository = repository;
-    }
 
     public void init() {
         entryMap.clear();
