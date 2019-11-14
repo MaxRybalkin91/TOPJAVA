336f5619dcd2185d9183380dbfdca807a94f6979
MaxRybalkin91 <granirock@gmail.com>
2019-11-14 23:13:28 +0300

    HW6. Controllers changed. Option-1 is done

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -5,107 +5,81 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealTo;
 
-import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 
 @Controller
 public class JspMealController extends AbstractMealController {
 
     @GetMapping("/meals")
-    public String getMeals(Model model, HttpServletRequest request) {
+    public String getMeals(Model model) {
         model.addAttribute("meals", getAll());
         return "meals";
     }
 
-    @GetMapping("/meals")
-    public String getMeal(HttpServletRequest request) {
-        final Meal meal = "create".equals(request.getParameter("action")) ?
-                new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
-                get(getId(request));
-        request.setAttribute("meal", meal);
-        return "mealForm";
-    }
+    @GetMapping("/meals/filter")
+    public String getFilteredMeals(
+            Model model,
+            @RequestParam String startDate,
+            @RequestParam String endDate,
+            @RequestParam String startTime,
+            @RequestParam String endTime
+    ) {
+        LocalDate parsedStartDate = parseLocalDate(startDate);
+        LocalDate parsedEndDate = parseLocalDate(endDate);
+        LocalTime parsedStartTime = parseLocalTime(startTime);
+        LocalTime parsedEndTime = parseLocalTime(endTime);
 
-    @PostMapping("/mealForm")
-    public String setMeal(HttpServletRequest request) {
-        Meal meal = new Meal(
-                LocalDateTime.parse(request.getParameter("dateTime")),
-                request.getParameter("description"),
-                Integer.parseInt(request.getParameter("calories")));
-
-        if (StringUtils.isEmpty(request.getParameter("id"))) {
-            create(meal);
-        } else {
-            update(meal, getId(request));
-        }
+        List<MealTo> filteredMeals = getBetween(parsedStartDate, parsedStartTime, parsedEndDate, parsedEndTime);
+        model.addAttribute("meals", filteredMeals);
         return "meals";
     }
 
-
-    /*
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        WebApplicationContext springContext = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
-        mealController = springContext.getBean(MealRestController.class);
+    @GetMapping("/meals/add")
+    public String addMeal(Model model) {
+        Meal meal = new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000);
+        model.addAttribute("meal", meal);
+        return "mealForm";
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        Meal meal = new Meal(
-                LocalDateTime.parse(request.getParameter("dateTime")),
-                request.getParameter("description"),
-                Integer.parseInt(request.getParameter("calories")));
+    @GetMapping("/meals/update")
+    public String editMeal(
+            Model model,
+            @RequestParam String id) {
+        Meal meal = get(Integer.parseInt(id));
+        model.addAttribute("meal", meal);
+        return "mealForm";
+    }
 
-        if (StringUtils.isEmpty(request.getParameter("id"))) {
-            mealController.create(meal);
+    @PostMapping("/meals/save")
+    public String saveMeal(
+            @RequestParam String id,
+            @RequestParam String dateTime,
+            @RequestParam String description,
+            @RequestParam String calories) {
+        Meal meal = new Meal(LocalDateTime.parse(dateTime), description, Integer.parseInt(calories));
+        if (!StringUtils.isEmpty(id)) {
+            update(meal, Integer.parseInt(id));
         } else {
-            mealController.update(meal, getId(request));
+            create(meal);
         }
-        response.sendRedirect("meals");
+        return "redirect:/meals";
     }
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String action = request.getParameter("action");
-
-        switch (action == null ? "all" : action) {
-            case "delete":
-                int id = getId(request);
-                mealController.delete(id);
-                response.sendRedirect("meals");
-                break;
-            case "create":
-            case "update":
-                final Meal meal = "create".equals(action) ?
-                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
-                        mealController.get(getId(request));
-                request.setAttribute("meal", meal);
-                request.getRequestDispatcher("/mealForm.jsp").forward(request, response);
-                break;
-            case "filter":
-                LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
-                LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
-                LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
-                LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
-                request.setAttribute("meals", mealController.getBetween(startDate, startTime, endDate, endTime));
-                request.getRequestDispatcher("/meals.jsp").forward(request, response);
-                break;
-            case "all":
-            default:
-                request.setAttribute("meals", mealController.getAll());
-                request.getRequestDispatcher("/meals.jsp").forward(request, response);
-                break;
-        }
+    @GetMapping("/meals/delete")
+    public String deleteMeal(
+            @RequestParam String id) {
+        delete(Integer.parseInt(id));
+        return "meals";
     }
-
-    private int getId(HttpServletRequest request) {
-        String paramId = Objects.requireNonNull(request.getParameter("id"));
-        return Integer.parseInt(paramId);
-    }
-     */
 }
--- src/main/webapp/WEB-INF/jsp/meals.jsp
+++ src/main/webapp/WEB-INF/jsp/meals.jsp
@@ -2,45 +2,46 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@ taglib prefix="fn" uri="http://topjava.javawebinar.ru/functions" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <html>
+<jsp:include page="fragments/headTag.jsp"/>
 <head>
     <title>Meals</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<jsp:include page="fragments/bodyHeader.jsp"/>
+<c:set var="homepage" value="${pageContext.request.contextPath}/"/>
 <section>
-    <h3><a href="index.jsp">Home</a></h3>
-    <hr/>
-    <h2>Meals</h2>
-    <form method="get" action="meals">
-        <input type="hidden" name="action" value="filter">
+    <h3><spring:message code="meal.title"/></h3>
+    <form method="get" action="${homepage}/meals/filter">
         <dl>
-            <dt>From Date:</dt>
+            <dt><spring:message code="meal.startDate"/></dt>
             <dd><input type="date" name="startDate" value="${param.startDate}"></dd>
         </dl>
         <dl>
-            <dt>To Date:</dt>
+            <dt><spring:message code="meal.endDate"/></dt>
             <dd><input type="date" name="endDate" value="${param.endDate}"></dd>
         </dl>
         <dl>
-            <dt>From Time:</dt>
+            <dt><spring:message code="meal.startTime"/></dt>
             <dd><input type="time" name="startTime" value="${param.startTime}"></dd>
         </dl>
         <dl>
-            <dt>To Time:</dt>
+            <dt><spring:message code="meal.endTime"/></dt>
             <dd><input type="time" name="endTime" value="${param.endTime}"></dd>
         </dl>
-        <button type="submit">Filter</button>
+        <button type="submit"><spring:message code="meal.filter"/></button>
     </form>
     <hr/>
-    <a href="meals?action=create">Add Meal</a>
+    <a href="${homepage}/meals/add"><spring:message code="meal.add"/></a>
     <br><br>
     <table border="1" cellpadding="8" cellspacing="0">
         <thead>
         <tr>
-            <th>Date</th>
-            <th>Description</th>
-            <th>Calories</th>
+            <th><spring:message code="meal.dateTime"/></th>
+            <th><spring:message code="meal.description"/></th>
+            <th><spring:message code="meal.calories"/></th>
             <th></th>
             <th></th>
         </tr>
@@ -49,18 +50,16 @@
             <jsp:useBean id="meal" type="ru.javawebinar.topjava.to.MealTo"/>
             <tr data-mealExcess="${meal.excess}">
                 <td>
-                        <%--${meal.dateTime.toLocalDate()} ${meal.dateTime.toLocalTime()}--%>
-                        <%--<%=TimeUtil.toString(meal.getDateTime())%>--%>
-                        <%--${fn:replace(meal.dateTime, 'T', ' ')}--%>
                         ${fn:formatDateTime(meal.dateTime)}
                 </td>
                 <td>${meal.description}</td>
                 <td>${meal.calories}</td>
-                <td><a href="meals?action=update&id=${meal.id}">Update</a></td>
-                <td><a href="meals?action=delete&id=${meal.id}">Delete</a></td>
+                <td><a href="${homepage}/meals/update?id=${meal.id}"><spring:message code="meal.update"/></a></td>
+                <td><a href="${homepage}/meals/delete?id=${meal.id}"><spring:message code="meal.delete"/></a></td>
             </tr>
         </c:forEach>
     </table>
 </section>
+<jsp:include page="fragments/footer.jsp"/>
 </body>
 </html>
